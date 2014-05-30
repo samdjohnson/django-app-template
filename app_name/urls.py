9cacbc3e514bb643978963f64b7db7b40b1b1e94
Sam Johnson <sjohns52@gmail.com>
2014-05-30 02:10:51 -0500

    first pass at an app structure

---
+++ app_name/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+    # url(r'^pages/$', PageListView.as_view()),
+    # url(r'^pages/(?P<page_slug>[\w-]+)/$', ScreenshotGroupListView.as_view()),
+    # url(r'^pages/(?P<slug>[\w-]+)/(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})/$,', \
+    #     ScreenshotGroupListView.as_view())
+)
