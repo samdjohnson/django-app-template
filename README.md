476bb0fdcc630228a430ecfdd0589fb7052b6389
Sam Johnson <sjohns52@gmail.com>
2015-03-21 10:40:11 -0500

    added usage. cleaned up views. added ghost fixture so folder will clone

---
+++ README.md
@@ -0,0 +1,3 @@
+## Usage
+
+django-admin.py startapp <app_name> . --template='https://github.com/samdjohnson/django-app-template/archive/master.zip'
--- app_name/urls.py
+++ app_name/urls.py
@@ -1,8 +1,5 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    # url(r'^pages/$', PageListView.as_view()),
-    # url(r'^pages/(?P<page_slug>[\w-]+)/$', ScreenshotGroupListView.as_view()),
-    # url(r'^pages/(?P<slug>[\w-]+)/(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})/$,', \
-    #     ScreenshotGroupListView.as_view())
+    # url(r'^pages/$', ListView.as_view()),
 )
